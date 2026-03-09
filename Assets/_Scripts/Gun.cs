0ec17630f3389088eb7cbc79cb48d78b748d7f01
Vincent-207 <vhannah2007@outlook.com>
2026-03-09 17:35:01 -0500

    Cleaned up dither code, started gun movement

---
+++ Assets/_Scripts/Gun.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float cooldownDuration, shootPower;
+    bool canFire = true;
+    [SerializeField] InputActionReference fire;
+    [SerializeField] Transform orientation;
+    Rigidbody rb;
+    void OnEnable()
+    {
+        fire.action.started += Shoot;
+    }
+    void OnDisable()
+    {
+        fire.action.started -= Shoot;
+        
+    }
+    
+
+    void Shoot(InputAction.CallbackContext context)
+    {
+        if(canFire)
+        {
+            rb.AddForce(-Camera.main.transform.forward * shootPower, ForceMode.Impulse);
+            StartCoroutine(Cooldown(cooldownDuration));
+        }
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    IEnumerator Cooldown(float duration)
+    {
+        canFire = false;
+        while(duration > 0)
+        {
+            duration -= Time.deltaTime;
+            yield return null;
+        }
+
+        canFire = true;
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, orientation.transform.forward * 5f, Color.red);
+    }
+}
