073bc1c597b44bd2acc35a2108740a660dc712ae
Vincent-207 <vhannah2007@outlook.com>
2026-03-09 18:28:44 -0500

    Capacity & reloading

--- Assets/_Scripts/Gun.cs
+++ Assets/_Scripts/Gun.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,28 +8,54 @@ using UnityEngine.InputSystem;
 public class Gun : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] float cooldownDuration, shootPower;
-    bool canFire = true;
-    [SerializeField] InputActionReference fire;
+    [SerializeField] float cooldownDuration, shootPower, reloadTime;
+    bool canFire = true, isReloading = false;
+    [SerializeField] InputActionReference fire, reload;
     [SerializeField] Transform orientation;
     [SerializeField] int maxCapacity, currentCapacity;
+    [SerializeField] TMP_Text debugText;
     Rigidbody rb;
     void OnEnable()
     {
         fire.action.started += Shoot;
+        reload.action.started += Reload;
     }
     void OnDisable()
     {
         fire.action.started -= Shoot;
+        reload.action.started -= Reload;
         
     }
     
-
+    void Reload(InputAction.CallbackContext context)
+    {
+        if(!isReloading) StartCoroutine(ReloadRoutine());
+    }
+    void UpdateCounter()
+    {
+        debugText.text = String.Format("{0} / {1}", currentCapacity, maxCapacity);
+    }
+    IEnumerator ReloadRoutine()
+    {
+        isReloading = true;
+        debugText.text = "Reloading...";
+        float duration = reloadTime;
+        while(duration > 0)
+        {
+            duration -=Time.deltaTime;
+            yield return null;
+        }
+        isReloading = false;
+        currentCapacity = maxCapacity;
+        UpdateCounter();
+    }
     void Shoot(InputAction.CallbackContext context)
     {
-        if(canFire)
+        if(canFire && (currentCapacity > 0 && !isReloading) )
         {
             rb.AddForce(-Camera.main.transform.forward * shootPower, ForceMode.Impulse);
+            currentCapacity--;
+            UpdateCounter();
             StartCoroutine(Cooldown(cooldownDuration));
         }
     }
@@ -35,10 +63,12 @@ public class Gun : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         currentCapacity = maxCapacity;
+        UpdateCounter();
     }
 
     IEnumerator Cooldown(float duration)
     {
+        
         canFire = false;
         while(duration > 0)
         {
